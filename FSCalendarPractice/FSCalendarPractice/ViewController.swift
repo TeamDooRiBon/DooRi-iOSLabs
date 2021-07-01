402fd69c8c36178a595ffc96b49f0f8b4b63568a
SangJin <70688424+Hansangjin98@users.noreply.github.com>
2021-07-01 18:01:31 +0900

    [ADD] FSCalendar 연습

---
+++ FSCalendarPractice/FSCalendarPractice/ViewController.swift
@@ -0,0 +1,119 @@
+//
+//  ViewController.swift
+//  FSCalendarPractice
+//
+//  Created by 한상진 on 2021/07/01.
+//
+
+import UIKit
+import FSCalendar
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var calendar: FSCalendar!
+
+    private var firstDate: Date?
+    private var lastDate: Date?
+    private var datesRange: [Date]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        topLabel.numberOfLines = 2
+        topLabel.text = "번들님 여행 날짜는\n언제인가요?"
+        calendar.today = nil
+        calendar.allowsMultipleSelection = true
+        calendar.scrollDirection = .vertical
+        calendar.swipeToChooseGesture.isEnabled = true
+        
+        calendar.appearance.weekdayTextColor = .systemGray2
+        calendar.appearance.headerTitleColor = .blue
+        calendar.appearance.headerDateFormat = "YYYY년 M월"
+        calendar.appearance.headerTitleFont = UIFont.systemFont(ofSize: 24)
+        calendar.locale = Locale(identifier: "ko_KR")
+        calendar.calendarWeekdayView.weekdayLabels[0].textColor = .red
+        calendar.calendarWeekdayView.weekdayLabels[6].textColor = .red
+    }
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        // nothing selected:
+        if firstDate == nil {
+            firstDate = date
+            datesRange = [firstDate!]
+
+            print("datesRange contains: \(datesRange!)")
+
+            return
+        }
+
+        // only first date is selected:
+        if firstDate != nil && lastDate == nil {
+            // handle the case of if the last date is less than the first date:
+            if date <= firstDate! {
+                calendar.deselect(firstDate!)
+                firstDate = date
+                datesRange = [firstDate!]
+
+                print("datesRange contains: \(datesRange!)")
+
+                return
+            }
+
+            let range = datesRange(from: firstDate!, to: date)
+
+            lastDate = range.last
+
+            for d in range {
+                calendar.select(d)
+            }
+
+            datesRange = range
+
+            print("datesRange contains: \(datesRange!)")
+
+            return
+        }
+
+        // both are selected:
+        if firstDate != nil && lastDate != nil {
+            for d in calendar.selectedDates {
+                calendar.deselect(d)
+            }
+
+            lastDate = nil
+            firstDate = nil
+
+            datesRange = []
+
+            print("datesRange contains: \(datesRange!)")
+        }
+    }
+
+    func calendar(_ calendar: FSCalendar, didDeselect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        // both are selected:
+
+        // NOTE: the is a REDUANDENT CODE:
+        if firstDate != nil && lastDate != nil {
+            for d in calendar.selectedDates {
+                calendar.deselect(d)
+            }
+
+            lastDate = nil
+            firstDate = nil
+
+            datesRange = []
+            print("datesRange contains: \(datesRange!)")
+        }
+    }
+    func datesRange(from: Date, to: Date) -> [Date] {
+        if from > to { return [Date]() }
+        var tempDate = from
+        var array = [tempDate]
+        while tempDate < to {
+            tempDate = Calendar.current.date(byAdding: .day, value: 1, to: tempDate)!
+            array.append(tempDate)
+        }
+        return array
+    }
+}
+
---
+++ FSCalendarPractice/Pods/Target Support Files/Pods-FSCalendarPractice/Pods-FSCalendarPractice-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FSCalendarPractice : NSObject
+@end
+@implementation PodsDummy_Pods_FSCalendarPractice
+@end
